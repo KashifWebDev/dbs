1e212fdb0d5aa8b6fc21f2ce1f24166aaa6d1a60
kmali <kmalik748@gmail.com>
2021-06-18 17:36:10 +0500

    12 July, 02:21 PM

--- create_user.php
+++ create_user.php
@@ -61,9 +61,9 @@ if (isset($_SESSION["darkTheme"]) && $_SESSION["darkTheme"] == 1){
 
 <!-- Page Content -->
     <div class="container mt-4">
-        <a href="create_user.php" class="btn btn-primary float-right mb-2">
+        <button type="button" class="btn btn-primary float-right mb-2" data-toggle="modal" data-target="#myModal">
             Create New User
-        </a>
+        </button>
 
         <table id="example" class="table table-striped table-bordered bg-white shadow autoColorTable_theme" style="width:100%">
             <thead>
