1f126625c414cef61992414b67261fe6a01d50c1
kmali <kmalik748@gmail.com>
2021-10-20 13:52:54 +0500

    d

--- api/api.php
+++ api/api.php
@@ -49,9 +49,9 @@ $Tor2 = $data->Tor2 == '' ? 0 : $data->Tor2;
 $dateNow = date('Y-m-d h:i:s', time());
 
     $sql = "INSERT INTO recorded_values (mac, legends1, legends2, legends3, legends4, legends5, legends6, t1, t2, t3, t4, r1, r2, r3, r4, l1, l2, l3, l4, analogue1, analogue2, analogue3, analogue4, analogue5, analogue6,
-            c1, c2, co1, co2, tor1, tor2, date_time)
+            c1, c2, co1, co2, tor1, tor2, date_time, raw)
             VALUES ('$mac', $D1, $D2, $D3, $D4, $D5, $D6, $T1, $T2, $T3, $T4, $R1, $R2, $R3, $R4, $L1, $L2, $L3, $L4, $A1, $A2, $A3, $A4, $A5, $A6,
-            $C1, $C2, $Col1, $Col2, $Tor1, $Tor2, '$dateNow')";
+            $C1, $C2, $Col1, $Col2, $Tor1, $Tor2, '$dateNow', '$data')";
     // echo $sql; die(); exti();
     if(mysqli_query($con, $sql)){
             echo json_encode(
